4b2cc587719dfcf7824891e29ee563e748046b03
VictorP <pankovych.v@gmail.com>
2019-07-01 23:08:10 +0300

    Logger + delete controller

--- Controller/Webhook/Delete.php
+++ Controller/Webhook/Delete.php
@@ -4,6 +4,7 @@ namespace SalesAndOrders\FeedTool\Controller\Webhook;
 
 use \Magento\Framework\App\Action\Action;
 use \SalesAndOrders\FeedTool\Model\WebHookFactory;
+use \SalesAndOrders\FeedTool\Model\Logger;
 
 class Delete extends Action
 {
@@ -14,6 +15,8 @@ class Delete extends Action
 
     protected $webhookFactory;
 
+    protected $logger;
+
     /**
      * @param \Magento\Framework\App\Action\Context $context
      * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
@@ -21,11 +24,13 @@ class Delete extends Action
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory,
-        WebHookFactory $webHookFactory
+        WebHookFactory $webHookFactory,
+        Logger $logger
     )
     {
         $this->resultJsonFactory = $resultJsonFactory;
         $this->webhookFactory = $webHookFactory;
+        $this->logger = $logger;
         parent::__construct($context);
     }
     /**
@@ -42,17 +47,22 @@ class Delete extends Action
         /**
          * We check isset webhookID and create factory of model WebHook and change flag is_deleted to =1
          */
+        $logger = $this->logger->create('delete_webhook_id_' . $webhookID, 'webhooks');
         if ($webhookID) {
             $webhook = $this->webhookFactory->create()->load($webhookID);
+            $logger->info('Load webhook with ID = ' . $webhookID);
             if ($webhook && $webhook->getId()) {
                 $webhook->setIsDeleted(1);
                 $webhook->save();
                 $text = __('Webhook with ID=%1 deleted', $webhookID);
+                $logger->info('Change deteted floag is_deleted to 1');
             }else{
                 $text = __('Webhook with ID=%1 s doesnt exist', $webhookID);
+                $logger->info('Webhook with ID=' . $webhookID . ' is doesnt exist');
             }
         }else{
             $text = __('Webhook params is doesnt exist');
+            $logger->info('Webhook params is doesnt exist');
         }
 
         $data = ['message' => $text];
