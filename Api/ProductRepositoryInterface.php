63cd4d3a40153c44ac7745acd240749dfae51f31
Sasha <amykulanynets@salesandorders.com>
2020-05-21 15:45:08 -0700

    product-api
    #comment Implemented API to get Products in SandO module by next saample url
    http://pro.loc/index.php/rest/default/V1/sando/feed-tool/catalog/products?searchCriteria[pageSize]=2

---
+++ Api/ProductRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Sales And Orders Feed Tool
+ * Copyright © 2019 S&O LLC. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+namespace SalesAndOrders\FeedTool\Api;
+
+/**
+ * @api
+ * @since 1.0.23
+ */
+interface ProductRepositoryInterface extends \Magento\Catalog\Api\ProductRepositoryInterface
+{
+}
