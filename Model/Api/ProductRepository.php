a0c2127f24dd4da63e242dcc062d2aec30a84787
Sasha <amykulanynets@salesandorders.com>
2020-06-22 19:18:46 -0700

    API extended

--- Model/Api/ProductRepository.php
+++ Model/Api/ProductRepository.php
@@ -1,16 +1,81 @@
 <?php
-
+/**
+ * Sales And Orders Feed Tool
+ * Copyright © 2019 S&O LLC. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
 
 namespace SalesAndOrders\FeedTool\Model\Api;
 
+use Magento\CatalogUrlRewrite\Model\ProductUrlPathGenerator;
+use Magento\Store\Model\ScopeInterface;
 
-class ProductRepository extends \Magento\Catalog\Model\ProductRepository
+class ProductRepository //extends \Magento\Catalog\Model\ProductRepository
 {
+    /**
+     * @@var \Magento\Catalog\Model\ProductRepository
+    */
+    protected $productRepositoory;
+
+    /**
+     * @@var \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    */
+    protected $scopeConfig;
+
+    /**
+     * @var \Magento\Catalog\Api\Data\ProductSearchResultsInterfaceFactory
+     */
+    protected $searchResultsFactory;
+
+    /**
+     * * @param \Magento\Catalog\Api\Data\ProductSearchResultsInterfaceFactory $searchResultsFactory
+    */
+    public function __construct(
+         \Magento\Catalog\Model\ProductRepository  $productRepositoory
+        ,\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        ,\Magento\Catalog\Api\Data\ProductSearchResultsInterfaceFactory $searchResultsFactory
+    ){
+        $this->productRepositoory = $productRepositoory;
+        $this->scopeConfig = $scopeConfig;
+        $this->searchResultsFactory = $searchResultsFactory;
+    }
+
     /**
      * @inheritdoc
      */
     public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
     {
-        return parent::getList($searchCriteria);
+
+                        // Get Product List by Search criteria
+        return $searchResult = $this->productRepositoory->getList($searchCriteria);
+
+
+        $res = [];
+        $seoUrlSuffix = $this->scopeConfig->getValue(
+            ProductUrlPathGenerator::XML_PATH_PRODUCT_URL_SUFFIX,
+            ScopeInterface::SCOPE_STORE,
+            1
+        );
+
+        // get SEO  URL suffix
+        $items = $searchResult->getItems();   /** @var Magento\Catalog\Model\ProductSearchResults $items */
+        foreach ($items as &$item) {
+            $itemData = $item->getData();
+
+            unset($itemData['attribute_set_id']);
+            unset($itemData['typeet_id']);
+            unset($itemData['has_options']);
+            unset($itemData['required_options']);
+            unset($itemData['created_at']);
+            unset($itemData['extension_attribute_stock_item_notify_stock_qty']);
+            unset($itemData['extension_attribute_stock_item_low_stock_date']);
+
+//            $item->setData($itemData);
+            $res[] = $itemData;
+        }
+        $searchResult = $this->searchResultsFactory->create();
+        $searchResult->setItems($items);
+        return $searchResult;
+
     }
 }
--- Plugin/Product/Type/Configurable/GetList.php
+++ Plugin/Product/Type/Configurable/GetList.php
@@ -7,26 +7,55 @@
 
 namespace SalesAndOrders\FeedTool\Plugin\Product\Type\Configurable;
 
+use Magento\CatalogUrlRewrite\Model\ProductUrlPathGenerator;
 use Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable;
-
+use Magento\Store\Model\ScopeInterface;
+use \Magento\Store\Model\StoreManagerInterface;
 /**
  * Comment is required here
  */
 class GetList
 {
     protected $configurableProduct;
+    /**
+     * @@var \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    protected $scopeConfig;
+
+    /**
+     * @@var \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    protected $storeManager;
 
     public function __construct(
         Configurable $configurableProduct
+        ,\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        ,\Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->storeManager = $storeManager;
         $this->configurableProduct = $configurableProduct;
     }
 
-    public function afterGetList($subject, $products, $searchCriteria)
+    /**
+     *
+     * @param \Magento\Catalog\Model\ProductRepository\Interceptor $subject
+     * @param \Magento\Catalog\Model\ProductSearchResults $products
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * @return \Magento\Catalog\Model\ProductSearchResults
+     */
+    public function afterGetList($subject, $products, $searchCriteria):\Magento\Catalog\Model\ProductSearchResults
     {
+
+        $storeId = $this->storeManager->getStore()->getId(); // get store Id
+        $seoUrlSuffix = $this->getSeoUrlSufix($storeId);     //get SEO Url Suffix
+
         $productsData = $products->getItems();
         if (!empty($productsData)) {
             foreach ($productsData as $id => $product) {
+                                                // set SeoURLSuffix
+                $this->updateUrlKeyWithSeoSuffix($product,$seoUrlSuffix);
+                                                // add configurable variants
                 $value = null;
                 $parent = $this->configurableProduct->getParentIdsByChild($id);
                 if (!empty($parent)) {
@@ -36,5 +65,27 @@ class GetList
             }
         }
         return $products;
+    }
+
+    /**
+     * @param int $storeId
+     * @return string
+    */
+    protected function getSeoUrlSufix($storeId){
+        return $this->scopeConfig->getValue(
+            ProductUrlPathGenerator::XML_PATH_PRODUCT_URL_SUFFIX,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+    /**
+     * Add $seoUrlSuffix tp url_key
+     * @param Magento\Catalog\Model\Product\Interceptor $productItem
+     * @param string $seoUrlSuffix
+    */
+    protected function updateUrlKeyWithSeoSuffix($productItem,$seoUrlSuffix){
+        $urlKey = $productItem->getData('url_key');
+        $urlKey = $urlKey.$seoUrlSuffix;
+        $productItem->setData('url_key',$urlKey);
     }
 }
