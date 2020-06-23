f9e54a8af0d7a39b0086b81b8139e9ccc2f59b6b
Sasha <amykulanynets@salesandorders.com>
2020-06-22 19:14:58 -0700

    Configuration extended

--- Model/Config/Source/Mode.php
+++ Model/Config/Source/Mode.php
@@ -1,1 +1,32 @@
 <?php
+/**
+ * Sales And Orders Feed Tool
+ * Copyright © 2019 S&O LLC. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+namespace SalesAndOrders\FeedTool\Model\Config\Source;
+
+class Mode implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 0, 'label' => __('Default')],
+            ['value' => 1, 'label' => __('Develop')],
+            ['value' => 2, 'label' => __('Staging')],
+        ];
+    }
+
+    public function getLabelByValue($value){
+        $list = $this->toOptionArray();
+        foreach ($list as $el){
+            $label =  $el['value'] == $value ? $el['label'] : null;
+            if($label){
+                return (string)$label;
+            }
+        }
+    }
+}
