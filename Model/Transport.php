0ebb4b2167f42003136fcbb8cc5acd82be1d30a3
Sasha <amykulanynets@salesandorders.com>
2021-04-15 00:37:12 +0300

    Added loger to write transport data to the file

--- Model/Transport.php
+++ Model/Transport.php
@@ -7,24 +7,36 @@
 
 namespace SalesAndOrders\FeedTool\Model;
 
+use SalesAndOrders\FeedTool\Model\Logger;
 /**
  * Comment is required here
  */
 class Transport
 {
+
+    protected $logger;
+
+    public function __construct(
+        Logger $logger
+    ) {
+        $this->logger = $logger;
+    }
+
     /**
      * @param  $endpointUrl
      * @param  array $postData
      * @param  bool $checkUrl
      * @return array
      */
+
     public function sendData($endpointUrl, $postData = [], $checkUrl = true)
     {
-        if ($checkUrl) {
-            $endpointUrl = preg_replace("(^https?://)", "", $endpointUrl);
-        }
-        //ob_start();
-        //$out = fopen('php://output', 'w');
+        $logger = $this->logger->create('endPoint_sendData_curl', 'sendData');
+        //if ($checkUrl) {
+        //    $endpointUrl = preg_replace("(^https?://)", "", $endpointUrl);
+        //}
+        ob_start();
+        $out = fopen('php://output', 'w');
         $curl = curl_init();
         $curlOptions = [
             CURLOPT_URL => $endpointUrl,
@@ -35,7 +47,7 @@ class Transport
             CURLOPT_SSL_VERIFYHOST => 0,
             CURLOPT_TIMEOUT => 300,
             CURLOPT_VERBOSE => true,
-            //CURLOPT_STDERR => $out,
+            CURLOPT_STDERR => $out,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => "POST",
             CURLOPT_POSTFIELDS => json_encode($postData),
@@ -44,21 +56,28 @@ class Transport
                 "Cache-Control: no-cache"
             ],
         ];
-        if ($checkUrl) {
-            $curlOption[CURLOPT_PORT] = 80;
-        }
+//        if ($checkUrl) {
+//            $curlOption[CURLOPT_PORT] = 80;
+//        }
 
         curl_setopt_array($curl, $curlOptions);
 
         $response = curl_exec($curl);
         $info = curl_getinfo($curl);
         $err = curl_error($curl);
-        //fclose($out);
-        //$debug = ob_get_clean();
-//       if (empty($response) && empty($err)) {
-//           $err = $debug;
-            //todo write error to file log instead of printing
-//       }
+        fclose($out);
+        $debug = ob_get_clean();
+
+        $logger->info('Response from  ' . $endpointUrl . ' URL');
+        $logger->info(print_r(json_decode($response),1 ));
+       if (empty($response) && empty($err)) {
+           $err = $debug;
+           $logger->err('ERROR');
+           $logger->err($err); // write error to file log instead of printing
+       }else{
+           $logger->err('Info');
+           $logger->info($info);
+       }
         curl_close($curl);
         return ['response' => $response, 'err' => $err];
     }
